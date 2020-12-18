96002720abe7a5e8b3b0a1c18bedc83788b95c9b
missxa <aliona.kharchenko@gmail.com>
2020-12-18 17:56:31 +0100

    brief howto

--- software/twist_to_pwm.py
+++ software/twist_to_pwm.py
@@ -1,9 +1,30 @@
+# Usage:
+
+
+# rosrun rosserial_python serial_node.py tcp
+# plugin wheelchair power
+
+# cd esp-wheelchair
+# python software/twist_to_pwm.py
+
+# start ROS joystick node - atk3 is for logitech joystick
+# roslaunch teleop_twist_joy teleop.launch joy_config:=atk3
+# hold 1 and left joystick to drive (hold 2 to drive faster)
+
+# OR
+
+# alternatively run keyboard control
+# rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+# read the commands for the keyboard!
+
+
+
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int16
 
-PWM_MIN = 10
-PWMRANGE = 160
+PWM_MIN = 5
+PWMRANGE = 30
 
 rospy.init_node("wheelchair_twist_converter")
 
