fe9a3e8f6c1adcd41f735279c2bd030f4dca4396
Ronggur Mahendra Widya Putra <ronggur.mwp@gmail.com>
2023-02-03 15:40:31 +0100

    refactor and remove unnecesarry code

--- software/tof_potential_field.py
+++ software/tof_potential_field.py
@@ -1,7 +1,7 @@
-# # Usage:
+# Usage:
 
-# # cd esp-wheelchair
-# # python3 software/tof_potential_field.py
+# cd esp-wheelchair
+# python3 software/tof_potential_field.py
 
 import rospy
 from std_msgs.msg import Float64
@@ -10,14 +10,8 @@ from geometry_msgs.msg import Twist
 import pcl
 import ros_numpy
 import numpy as np
-# import pcl_conversions
-# import pcl.pcl_conversions
 import pcl.pcl_visualization
 
-# outputLinear = 0.0
-# outputAngular = 0.0
-# inputLinear = 0.0
-# inputAngular = 0.0
 emergencyStopThreshold = 0.1
 viewer = pcl.pcl_visualization.PCLVisualizering()
     
@@ -25,7 +19,7 @@ visualDone = False
 
 def point_cloud_callback(msg):
     
-    # chaneg from pointcloud2 to numpy
+    # change from pointcloud2 to numpy
     pc = ros_numpy.numpify(msg)
     height = pc.shape[0]
     width = pc.shape[1]
@@ -41,49 +35,27 @@ def point_cloud_callback(msg):
     # find the nearest point
     global minDist, maxDist
     minDist = 99999
-    maxDist = -99999
-    # print("np_points length :", len(np_points) / width)
-    
+
     for i in range(len(np_points)):
         if(minDist > np_points[i][2]):
             minDist = np_points[i][2]
-        if(maxDist < np_points[i][2]):
-            maxDist = np_points[i][2]
-    # minDist *=10
-    rospy.loginfo("max: %f min : %f", maxDist, minDist )
 
-    # if minDist > 0.7: #thresholding maximum speed
-    #     minDist = 0.7
-    # if minDist < 0: #thresholding minimum speed
-    #     minDist = 0
-    # global outputLinear
-    # rospy.loginfo("width: %f height : %f", width,height )
-    # if inputLinear < 0:
-    #     outputLinear = -1 * minDist
-    # elif inputLinear > 0:
-    #     outputLinear = minDist
-    # else : 
-    #     outputLinear = 0
-    
-    # outputspeed = minDist
+    rospy.loginfo("min : %f", minDist )
     if minDist < emergencyStopThreshold: # emergency stop
         outputspeed = 0.0
     
-
-
 def user_input_callback(msg):
     global inputLinear, inputAngular, outputAngular
     inputLinear = msg.linear.x
     inputAngular = msg.angular.z
-    # print("inputLinear : ", inputLinear, "inputAngular  : ",  inputAngular)
     outputAngular = inputAngular
     outputLinear = inputLinear
     if(minDist < emergencyStopThreshold and inputLinear > 0): # asume that this is the front ToF
-        rospy.loginfo ("ABOUT TO COLLIDE EMERGENCY BRAKE")
+        rospy.loginfo ("ABOUT TO COLLIDE FRONT EMERGENCY BRAKE")
         outputLinear = 0
     
     elif (minDist < emergencyStopThreshold and inputLinear < 0): # asume that this is the back ToF
-        rospy.loginfo ("ABOUT TO COLLIDE EMERGENCY BRAKE")
+        rospy.loginfo ("ABOUT TO COLLIDE BACK EMERGENCY BRAKE")
         outputLinear = 0
 
     twist = Twist()
@@ -96,33 +68,11 @@ def user_input_callback(msg):
     
 
 # def main():
-rospy.init_node('tof_potential_field')
+rospy.init_node('tof_emergency_brake')
 
 potential_field_pub = rospy.Publisher('/roboy/pinky/middleware/espchair/wheels/potential_field', Twist, queue_size=10)
 user_input_sub = rospy.Subscriber('/cmd_vel', Twist, user_input_callback)
 point_cloud_sub = rospy.Subscriber('/royale_camera_driver/point_cloud', PointCloud2, point_cloud_callback)
 
-
-# sub = rospy.Subscriber("/roboy/pinky/middleware/espchair/wheels/potential_field", Twist, cb)
-
-# rate = rospy.Rate(10)
-# while not rospy.is_shutdown():
-#     twist = Twist()
-#     print("outputLinear : ", outputLinear, "outputAngular  : ",  outputAngular)
-#     twist.linear.x = outputLinear
-#     twist.angular.z = outputAngular
-#     potential_field_pub.publish(twist)
-
-#     rospy.loginfo("publishing to /roboy/pinky/middleware/espchair/wheels/potential_field. Spinning...")
-#     # rospy.spin()
-#     rate.sleep()
-
-
-
-
 rospy.loginfo("publishing to /roboy/pinky/middleware/espchair/wheels/potential_field. Spinning...")
 rospy.spin()
-# rate.sleep()
-
-# if __name__ == '__main__':
-#     main()
