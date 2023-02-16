9558472ea1d9fac9c51f1f0f75ad7d07fdc9a32d
Ronggur Mahendra Widya Putra <ronggur.mwp@gmail.com>
2023-02-16 03:42:03 +0100

    refactor : use function at point cloud callback

--- src/tof.py
+++ src/tof.py
@@ -26,48 +26,48 @@ inputAngular = None
 manualMode = ManualMode()
 repelentMode = RepelentMode()
 
+def pointCloud_to_NumpyArray(point_Cloud):
+    height = point_Cloud.shape[0]
+    width = point_Cloud.shape[1]
+    np_points = np.zeros((height * width, 3), dtype=np.float32)
+    np_points[:, 0] = np.resize(point_Cloud['x'], height * width)
+    np_points[:, 1] = np.resize(point_Cloud['y'], height * width)
+    np_points[:, 2] = np.resize(point_Cloud['z'], height * width)
+    return np_points
+
+def visualizePointCloud(viewer ,point_Cloud):
+    if(useVisual):
+        p = pcl.PointCloud(np.array(point_Cloud, dtype=np.float32))
+        viewer.AddPointCloud(p, b'scene_cloud_front', 0)
+        viewer.SpinOnce()
+        viewer.RemovePointCloud( b'scene_cloud_front', 0)
+
+
 def point_cloud_front_callback(msg):
     # change from pointcloud2 to numpy
     pc = ros_numpy.numpify(msg)
-    height = pc.shape[0]
-    width = pc.shape[1]
-    np_points = np.zeros((height * width, 3), dtype=np.float32)
-    np_points[:, 0] = np.resize(pc['x'], height * width)
-    np_points[:, 1] = np.resize(pc['y'], height * width)
-    np_points[:, 2] = np.resize(pc['z'], height * width)
-    if(useVisual):
-        p = pcl.PointCloud(np.array(np_points, dtype=np.float32))
-        viewer_front.AddPointCloud(p, b'scene_cloud_front', 0)
-        viewer_front.SpinOnce()
-        viewer_front.RemovePointCloud( b'scene_cloud_front', 0)
+    front_Pointcloud_array = pointCloud_to_NumpyArray(pc)
+    visualizePointCloud(viewer_front, front_Pointcloud_array)
     # find the nearest point
-    global minDist_front
-    minDist_front =  np.amin(np_points[:, 2])
+    minDist_front =  np.amin(front_Pointcloud_array[:, 2])
 
 def point_cloud_back_callback(msg):
     # change from pointcloud2 to numpy
     pc = ros_numpy.numpify(msg)
-    height = pc.shape[0]
-    width = pc.shape[1]
-    np_points = np.zeros((height * width, 3), dtype=np.float32)
-    np_points[:, 0] = np.resize(pc['x'], height * width)
-    np_points[:, 1] = np.resize(pc['y'], height * width)
-    np_points[:, 2] = np.resize(pc['z'], height * width)
-    if(useVisual):
-        p = pcl.PointCloud(np.array(np_points, dtype=np.float32))
-        viewer_back.AddPointCloud(p, b'scene_cloud_back', 0)
-        viewer_back.SpinOnce()
-        viewer_back.RemovePointCloud( b'scene_cloud_back', 0)
+    back_Pointcloud_array = pointCloud_to_NumpyArray(pc)
+    visualizePointCloud(viewer_back, back_Pointcloud_array)
     # find the nearest point
-    global minDist_back
-    minDist_back = np.amin(np_points[:, 2])
+    minDist_back = np.amin(back_Pointcloud_array[:, 2])
 
 def user_input_callback(msg):   
     inputLinear = msg.linear.x
     inputAngular = msg.angular.z
 
-# manual mode
+    # manual mode
     outputLinear,outputAngular = manualMode.control(inputLinear,inputAngular)
+
+    # repelent field 
+    # outputLinear,outputAngular = repelentMode.control(inputLinear,inputAngular,minDist_front,minDist_back)
 
     if(minDist_front < emergencyStopThreshold and inputLinear > 0): # asume that this is the front ToF
         print ("ABOUT TO COLLIDE FRONT EMERGENCY BRAKE")
@@ -76,19 +76,16 @@ def user_input_callback(msg):
         print ("ABOUT TO COLLIDE BACK EMERGENCY BRAKE")
         outputLinear = 0
 
-# reppelent field 
-    # outputLinear,outputAngular = repelentMode.control(inputLinear,inputAngular,minDist_front,minDist_back)
-
     twist = Twist()
     twist.linear.x = outputLinear
     twist.angular.z = outputAngular
     assisted_navigation_pub.publish(twist)
 
+# main loop
 rospy.init_node('assisted_Navigation')
 
 assisted_navigation_pub = rospy.Publisher('/roboy/pinky/middleware/espchair/wheels/assisted_navigation', Twist, queue_size=10)
 user_input_sub = rospy.Subscriber('/cmd_vel', Twist, user_input_callback)
-# point_cloud_sub = rospy.Subscriber('/royale_camera_driver/point_cloud', PointCloud2, point_cloud_callback)
 
 point_cloud_front_sub = rospy.Subscriber('/tof1_driver/point_cloud', PointCloud2, point_cloud_front_callback)
 point_cloud__back_sub = rospy.Subscriber('/tof2_driver/point_cloud', PointCloud2, point_cloud_back_callback)
