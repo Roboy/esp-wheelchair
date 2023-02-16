31155ec552cdd34574fd01d5d5f766a7ba7068c5
Ronggur Mahendra Widya Putra <ronggur.mwp@gmail.com>
2023-02-16 03:24:28 +0100

    refactor : add comment to bash file

--- scripts/assistedNavigation.bash
+++ scripts/assistedNavigation.bash
@@ -1,6 +1,8 @@
-#export ROS_HOSTNAME=192.168.0.124
+# this script run all the necessary setup to run the assisted navigation program using the teleop keyboard
+
+export ROS_HOSTNAME=192.168.0.124
 # if you use other ip for ros master then do  “export ROS_MASTER_URI=http://{ROS_MASTER_IP}:11311”
-#export ROS_MASTER_URI=http://192.168.0.124:11311
+export ROS_MASTER_URI=http://192.168.0.124:11311
 roscore &
 python3 software/twist_to_pwm.py &
 python3 software/tof.py & 
--- src/README.md
+++ src/README.md
@@ -1,3 +1,6 @@
 # Assisted Navigation
 
+
+## How to Run 
 ToF documentation can be accsessed here : https://devanthro.atlassian.net/wiki/spaces/WS2223/pages/2747269188/AN+Software
+
