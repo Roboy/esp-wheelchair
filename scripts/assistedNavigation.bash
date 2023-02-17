61e36d87bab62e1dcb801175fd5f4b471c10c6e9
ronggurmahendra <ronggur.mwp@gmail.com>
2023-02-17 14:42:56 +0100

    refactor : move test file

--- scripts/assistedNavigation.bash
+++ scripts/assistedNavigation.bash
@@ -4,6 +4,5 @@ export ROS_HOSTNAME=192.168.0.124
 # if you use other ip for ros master then do  “export ROS_MASTER_URI=http://{ROS_MASTER_IP}:11311”
 export ROS_MASTER_URI=http://192.168.0.124:11311
 roscore &
-python3 software/twist_to_pwm.py &
-python3 software/tof.py & 
+python3 ../src/tof.py & 
 rosrun teleop_twist_keyboard teleop_twist_keyboard.py
